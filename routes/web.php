782aed707a5d0a6889cead2435f6503b49e16118
wahliyudin <wahliyudinzein@gmail.com>
2022-06-12 08:58:43 +0700

    Fix

--- routes/web.php
+++ routes/web.php
@@ -23,7 +23,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.exports.struk');
     return redirect()->route('login');
 });
 Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->group(function () {
